9a1146aad1e8adf209ef0feeb7c2e006589f3a96
Vijay Reddy <venna0033@gmail.com>
2024-06-21 15:54:51 +0530

    added new schedule time and view button models

--- 0/index.php
+++ 0/index.php
@@ -323,6 +323,8 @@ include(BASE_PATH."dashboard/dashboard_modals.php");
 </main>
 <script src="<?php echo BASE_PATH;?>assets/js/sidebar-menu.js"></script>
 <script src="<?php echo BASE_PATH;?>js_modal_scripts/dashboard_modals_script.js"></script>
+<script src="<?php echo BASE_PATH;?>js_modal_scripts/viewbutton.js"></script>
+
 <?php
 include(BASE_PATH."assets/html/body-end.php"); 
 include(BASE_PATH."assets/html/html-end.php"); 
--- dashboard/modals/Active_modal.php
+++ dashboard/modals/Active_modal.php
@@ -13,10 +13,9 @@
                                 <tr>
                                     <th class="bg-logo-color text-white" scope="col">Device ID</th>
                                     <th class="bg-logo-color text-white" scope="col">Device Name</th>
-
                                     <th class="bg-logo-color text-white" scope="col">Location</th>
-
                                     <th class="bg-logo-color text-white" scope="col">Last Updated</th>
+                                    <th class="bg-logo-color text-white" scope="col">Status</th>
                                     
                                 </tr>
                             </thead>
@@ -27,30 +26,35 @@
                                     
                                     <td><a href="https://www.google.com/maps?q=17.467754,%2078.603072" target="_blank">Location</a></td>
                                     <td>2024-06-09</td>
+                                    <td><button class="btn btn-info btn-sm" onclick="openOpenviewModal()" >View</button></td>
                                 </tr>   
                                 <tr>
                                     <td>Device 2</td>
                                     <td> CCMS 1</td>
                                     <td><a href="https://www.google.com/maps?q=17.467754,%2078.603072" target="_blank">Location</a></td>
                                     <td>2024-06-09</td>
+                                    <td><button class="btn btn-info btn-sm" onclick="openOpenviewModal()" >View</button></td>
                                 </tr> 
                                 <tr>
                                     <td>Device 3</td>
                                     <td> CCMS 1</td>
                                     <td><a href="https://www.google.com/maps?q=17.467754,%2078.603072" target="_blank">Location</a></td>
                                     <td>2024-06-09</td>
+                                    <td><button class="btn btn-info btn-sm" onclick="openOpenviewModal()" >View</button></td>
                                 </tr>    
                                 <tr>
                                     <td>Device 3</td>
                                     <td> CCMS 1</td>
                                     <td><a href="https://www.google.com/maps?q=17.467754,%2078.603072" target="_blank">Location</a></td>
                                     <td>2024-06-09</td>
+                                    <td><button class="btn btn-info btn-sm" onclick="openOpenviewModal()" >View</button></td>
                                 </tr> 
                                 <tr>
                                     <td>Device 3</td>
                                     <td> CCMS 1</td>
                                     <td><a href="https://www.google.com/maps?q=17.467754,%2078.603072" target="_blank">Location</a></td>
                                     <td>2024-06-09</td>
+                                    <td><button class="btn btn-info btn-sm" onclick="openOpenviewModal()" >View</button></td>
                                 </tr>  
                             </tbody>
                         </table>
@@ -63,3 +67,7 @@
         </div>
     </div>
 </div>
+ <?php
+// include("../js_modal_scripts/viewbutton.js");
+include("viewbutton_modal.php");
+?> 
